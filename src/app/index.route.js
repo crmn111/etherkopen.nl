8e53a6c3b9b90ec032a0f5f2742168d2fa74eed4
Dirk <dirkjan@degroot.in>
2016-06-10 15:39:58 +0200

    new design

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,26 +1,37 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('etherkopen2')
-    .config(routerConfig);
+    angular
+        .module('etherkopen2')
+        .config(routerConfig);
 
-  /** @ngInject */
-  function routerConfig($stateProvider, $urlRouterProvider) {
-    $stateProvider
-      .state('home', {
-        url: '/',
-        templateUrl: 'app/main/main.html',
-        controller: 'MainController',
-        controllerAs: 'vm'
-      }).state('manual', {
-          url: '/uitleg-ethereum-wallet-installeren-en-daarna-ether-kopen',
-          templateUrl: 'app/content/manual.html',
-          controller: 'ManualController',
-          controllerAs: 'vm'
-        });
+    /** @ngInject */
+    function routerConfig($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/main/main.html',
+                controller: 'MainController',
+                controllerAs: 'vm',
+                data: {
+                    pageTitle: 'Ether kopen (Ethereum) met Bitcoin',
+                    pageDescription : 'Ether (ETH) is een verhandelbaar token/asset - U kunt hier gemakkelijk Ether (Ethereum) kopen met Bitcoin.'
+                }
+            }).state('manual', {
+                url: '/uitleg-ethereum-wallet-installeren-en-daarna-ether-kopen',
+                templateUrl: 'app/content/manual.html',
+                controller: 'ManualController',
+                controllerAs: 'vm',
+                data: {
+                    pageTitle: 'Handleiding Ether Kopen',
+                    pageDescription : 'Uitleg Ethereum wallet (Mist) installeren en ether kopen.'
+                }
+            }).state('chart', {
+                url: '/chart/btc-eth',
+                templateUrl: 'app/chart/chart.html'
+            });
 
-    $urlRouterProvider.otherwise('/');
-  }
+        $urlRouterProvider.otherwise('/');
+    }
 
 })();
--- src/app/index.run.js
+++ src/app/index.run.js
@@ -6,8 +6,15 @@
     .run(runBlock);
 
   /** @ngInject */
-  function runBlock($log) {
+  function runBlock($rootScope,$state) {
 
+    $rootScope.$state = $state;
+
+    $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+
+      document.body.scrollTop = document.documentElement.scrollTop = 0;
+      $rootScope.$statename = $state.$current.name.replace('.','-');
+    });
 
   }
 
